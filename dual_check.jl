4ca8b799206efe697700cdec90797b44e901ed9c
rjustincardona <rjustincardona@gmail.com>
2024-09-17 12:16:55 -0400

    variable window size

--- dual_check.jl
+++ dual_check.jl
@@ -1,42 +1,53 @@
 include("constraint_function.jl")
-using BaryRational, ProgressMeter
+using BaryRational
 
 
-function find_zero(C::ConstraintFunction{T}; z_init::T=T(1.0), l_init::Int=typemax(Int), inc::Int=1, domain=zeros(T, 1), codomain=zeros(real(T), 1)) where T
+function find_zero(C::ConstraintFunction{T}; z_init::T=T(1.0), l_init::Int=10, domain=zeros(T, 1), codomain=zeros(real(T), 1), window::Int=10, points::Int=0, width::T=T(1)) where T
 	err = typemax(real(T))
-	z = real(z_init)
+	z = z_init
 	if length(domain) == 1
-		domain = rand(real(T), floor(Int, length(C))) .+ z
+		# domain = width .* randn(real(T), l_init) .+ real(T)(z)
+		domain = width .* (rand(real(T), l_init) .- 0.5) .+ real(T)(z)
 		codomain = map(x-> C*T(x), domain)
 	end
+    
+	i = 0
+	while true
+		err > cbrt(eps(real(T))) || break
+		i += 1
 
-	@showprogress desc=norm(C*z) for _ in range(1, l_init)
-		domain_add = rand(real(T), floor(Int, inc)) .+ z
-		domain = vcat(domain, domain_add)
-		codomain = vcat(codomain, map(x-> C*T(x), domain_add))
+		z = rand(real(T)) + real(T)(z - 0.5)
+		window = min(window, length(domain))
+		domain = vcat(domain, [z])[end-window:end]
+		codomain = vcat(codomain, [C*T(z)])[end-window:end]
 		
 		a = aaa(domain, codomain, clean=1)
 		_, _, zeros = prz(a)
-		z = T(maximum(real.(zeros)))
-		if norm(C * z) < sqrt(eps(real(T)))
-			break
-		else
-			println(norm(C*z))
+		zeros = real.(zeros)
+		try
+			z1 = real(T)(zeros[end])
+			z2 = real(T)(zeros[argmin(abs.(zeros .- z))])
+			z = T(min(z1, z2))
+			println("\t", i, ": ", norm(C*z), " at ", z)
+			println("\t", zeros)
+			readline()
+			err = norm(C * z)
+		catch
+			println("\tRestarting after ", i, " iterations.")
+			return find_zero(C, z_init=z, l_init=l_init, window=window, points=i)
 		end
 	end
-	return real(z), err, domain, codomain
+	return real(z), err, domain, codomain, points+i
 end
 
 
-function optimize(C::ConstraintFunction{T}; z_init::T=T(1.0), inc::Int=1, iter::Int=10) where T
+function optimize(C::ConstraintFunction{T}; l_init::Int=typemax(Int), z_init::T=T(1.0), iter::Int=10, dx::T=T(1e-3)) where T
 	errs = Array{real(T)}(undef, iter, 2)
-	z, e, domain, codomain = find_zero(C, z_init=z_init, inc=inc)
 	for i in range(1, iter)
 		println("Depth ", i)
-		errs[i, :] = [e, length(domain)]
-		z, e, domain, codomain = find_zero(C, z_init=T(z), l_init=1, inc=inc, domain=domain, codomain=codomain)
-		perturb(C, T(1e-3))
+		z, e, domain, codomain, p = find_zero(C, l_init=Int(floor(l_init * (0.5 + i/2))), z_init=z_init, width=dx)
+		errs[i, :] = [e, real(T)(p)]
+		perturb(C, dx)
 	end
 	return errs
 end
-
--- test.jl
+++ test.jl
@@ -1,9 +1,21 @@
 include("dual_check.jl")
-using Plots
+using Plots, Random
 
-C = ConstraintFunction(2)
-x = ComplexF32.(range(1, 10))
-y = map(z-> C * z, x)
-plot(Float32.(x), y)
-savefig("preview.png")
-# optimize(C)
+Random.seed!(1)
+CURAND.seed!(1)
+# C = ConstraintFunction(2)
+
+# x = range(247.592, 247.597, 100)
+# y = map(z-> C * ComplexF32(z), x)
+# plot(x, y)
+# savefig("preview.png")
+
+dx = ComplexF32.([1e-4])
+l_init = [5]
+params = collect(Iterators.product(dx, l_init))
+
+for p in params
+	C = ConstraintFunction(2)
+	o = optimize(C, z_init=ComplexF32(247.5949), dx = p[1], l_init=p[2], iter=5)
+	println(o)
+end
