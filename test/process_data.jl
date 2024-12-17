7cf469737f04a92ccfcc8a97cffa6f24ebe8312e
Justin Cardona <cardonajustin>
2024-12-17 17:37:58 -0500

    about to edit thesis ppt

--- test/process_data.jl
+++ test/process_data.jl
@@ -10,7 +10,7 @@ ms = mean(ns, dims=1)'
 ss = std(ns, dims=1)'
 plot(LinRange(1, 10, 10), ms, ribbon=ss, fillalpha=.5, legend=false, xlabel="Number of Perturbations", ylabel="Expected Number of Inverse Solves")
 savefig("data/perturbation_iters.png")
-histogram(ns[:], bins=5:1:25, legend=false)
+histogram(ns[:], bins=5:1:25, yaxis = (:log10), legend=false)
 savefig("data/histogram.png")
 @show mean(ns[:])
 @show std(ns[:])
